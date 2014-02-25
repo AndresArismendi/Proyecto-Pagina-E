bc2e34b4b2a4a7e55be35f94d3a1d98b680dc2b3
Fabianko <fabian.arismendi@usach.cl>
2014-02-25 16:31:40 -0400

    creando proyecto en ruby
    el proyecto en ruby sin ninguna modificación

---
+++ paginaMono/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaginaMono::Application.config.secret_key_base = '21cd6d16a6a0faa431a0106476b733d8538e0c2c82dfc73ba51765bcbe6e95259a37201e0cc00f50b7580c964c9fb0483784fc100f6a0f5ef3a3f5920ac83d68'
